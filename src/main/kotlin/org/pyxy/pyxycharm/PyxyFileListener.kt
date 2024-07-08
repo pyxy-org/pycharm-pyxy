31aec3c7e326625f6c00a9250da8891f3959b2f0
treykeown <2755914+treykeown@users.noreply.github.com>
2024-07-08 00:31:42 -0400

    Auto-convert .pyxy to .py

--- src/main/kotlin/org/pyxy/pyxycharm/PyxyFileListener.kt
+++ src/main/kotlin/org/pyxy/pyxycharm/PyxyFileListener.kt
@@ -1,12 +1,18 @@
 package org.pyxy.pyxycharm
 
+import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.execution.util.ExecUtil.execAndGetOutput
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.ProjectLocator
 import com.intellij.openapi.vfs.AsyncFileListener
 import com.intellij.openapi.vfs.AsyncFileListener.ChangeApplier
+import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent
+import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
 import com.intellij.util.concurrency.AppExecutorUtil
+import com.jetbrains.python.sdk.PythonSdkUtil
 
 
 class PyxyFileListener : AsyncFileListener {
@@ -23,9 +29,29 @@ class PyxyFileListener : AsyncFileListener {
                             val psiFile = PsiManager.getInstance(project).findFile(file) ?: continue
                             if (!psiFile.language.isKindOf(PyxyLanguageDialect)) continue
                             println("${project.name} ${file.path}")
+                            compileWithPyxy(psiFile)
                         }
                     }
                 }
+            }
+        }
+    }
+
+    fun compileWithPyxy(psiFile: PsiFile) {
+        val pythonSdk = PythonSdkUtil.findPythonSdk(psiFile) ?: return
+        val commandLine = GeneralCommandLine()
+            .withExePath(pythonSdk.homePath!!)
+            .withParameters("-m", "pyxy", psiFile.virtualFile.path)
+            .withWorkDirectory(psiFile.project.basePath)
+        ApplicationManager.getApplication().executeOnPooledThread {
+            try {
+                val output = execAndGetOutput(commandLine)
+                Logger.getInstance(PyxyFileListener::class.java).warn("stdout: " + output.stdout)
+                Logger.getInstance(PyxyFileListener::class.java).warn("stderr: " + output.stderr)
+                Logger.getInstance(PyxyFileListener::class.java).warn("exitcode: " + output.exitCode)
+                VfsUtil.markDirtyAndRefresh(true, false, true, psiFile.virtualFile.parent);
+            } catch (e: Exception) {
+                Logger.getInstance(PyxyFileListener::class.java).error("Error running module foo", e);
             }
         }
     }
