8879392d4ccda74c77d6613659b3ef3aa37172c4
treykeown <2755914+treykeown@users.noreply.github.com>
2024-07-09 15:45:22 -0400

    Fix test and PyxyTagExpression

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Pyxy support for PyCharm
 ## TODO
 
 * [ ] Fix bug on hyphenated tag names
+  * Partially fixed, need to figure out how to handle tag names which aren't valid identifiers (due to containing a hyphen, colon, or otherwise)
 * [ ] Validate closing tag name
 * [ ] Highlight matching tag pairs
 * [ ] Colorize nested tags (like is done for HTML files)
--- src/main/kotlin/org/pyxy/pyxycharm/psi/element/PyxyTagExpression.kt
+++ src/main/kotlin/org/pyxy/pyxycharm/psi/element/PyxyTagExpression.kt
@@ -2,17 +2,22 @@ package org.pyxy.pyxycharm.psi.element
 
 import com.intellij.lang.ASTNode
 import com.jetbrains.python.PyElementTypes
+import com.jetbrains.python.ast.findChildByType
 import com.jetbrains.python.psi.PyArgumentList
 import com.jetbrains.python.psi.PyExpression
 import com.jetbrains.python.psi.impl.PyCallExpressionImpl
 
 class PyxyTagExpression(node: ASTNode) : PyCallExpressionImpl(node) {
     override fun getCallee(): PyExpression? {
-        return findChildByType(PyElementTypes.REFERENCE_EXPRESSION)
+        val openingTag = findChildByType<PyxyTag>(PyxyElementTypes.TAG) ?: return null
+        val tagName = openingTag.node.findChildByType(PyxyElementTypes.TAG_NAME) ?: return null
+        return tagName.psi as PyExpression
     }
 
     override fun getArgumentList(): PyArgumentList? {
-        return findChildByType(PyElementTypes.ARGUMENT_LIST)
+        val openingTag = findChildByType<PyxyTag>(PyxyElementTypes.TAG) ?: return null
+        val tagName = openingTag.node.findChildByType(PyElementTypes.ARGUMENT_LIST) ?: return null
+        return tagName.psi as PyArgumentList
     }
 
     override fun toString(): String {
