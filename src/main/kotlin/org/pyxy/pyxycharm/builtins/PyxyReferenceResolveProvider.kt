e221761734b9219fec1d8541647bf4a1932cbec1
treykeown <2755914+treykeown@users.noreply.github.com>
2024-07-09 00:34:47 -0400

    Lookup in htpy if the reference doesn't resolve normally

--- src/main/kotlin/org/pyxy/pyxycharm/builtins/PyxyReferenceResolveProvider.kt
+++ src/main/kotlin/org/pyxy/pyxycharm/builtins/PyxyReferenceResolveProvider.kt
@@ -1,23 +1,35 @@
 package org.pyxy.pyxycharm.builtins
 
-import com.intellij.psi.PsiManager
-import com.jetbrains.python.psi.PyFile
 import com.jetbrains.python.psi.PyQualifiedExpression
 import com.jetbrains.python.psi.resolve.PyReferenceResolveProvider
 import com.jetbrains.python.psi.resolve.RatedResolveResult
+import com.jetbrains.python.psi.stubs.PyModuleNameIndex
 import com.jetbrains.python.psi.types.TypeEvalContext
 import org.pyxy.pyxycharm.PyxyLanguageDialect
+import org.pyxy.pyxycharm.psi.element.PyxyTagName
 
 class PyxyReferenceResolveProvider : PyReferenceResolveProvider {
     override fun resolveName(expression: PyQualifiedExpression, context: TypeEvalContext): List<RatedResolveResult> {
         if (expression.containingFile.language !is PyxyLanguageDialect) {
             return emptyList()
         }
-        val builtinsFile = PsiManager.getInstance(expression.project).findFile(
-            PYXY_BUILTINS
-        ) as? PyFile ?: return emptyList()
+        if (expression !is PyxyTagName) {
+            return emptyList()
+        }
+        println("resolveName with PyxyTagName: ${expression.text}")
+        println(expression.getReference().javaClass.name)
+
+        // Lookup htpy module
+        val matchingModules = PyModuleNameIndex.findByShortName("htpy", expression.project, expression.resolveScope)
+        if (matchingModules.isEmpty()) {
+            return emptyList()
+        }
+        val htpyModule = matchingModules[0]
+
+        // Lookup expression name in htpy
+        htpyModule.findExportedName(expression.name)
         return listOfNotNull(
-            builtinsFile.findExportedName(expression.name)?.let {
+            htpyModule.findExportedName(expression.name)?.let {
                 RatedResolveResult(RatedResolveResult.RATE_NORMAL, it)
             },
         )
--- src/main/kotlin/org/pyxy/pyxycharm/psi/element/PyxyTagName.kt
+++ src/main/kotlin/org/pyxy/pyxycharm/psi/element/PyxyTagName.kt
@@ -1,29 +1,12 @@
 package org.pyxy.pyxycharm.psi.element
 
 import com.intellij.lang.ASTNode
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiPolyVariantReference
-import com.intellij.psi.util.PsiTreeUtil
-import com.jetbrains.python.PythonRuntimeService
 import com.jetbrains.python.psi.PyElementVisitor
-import com.jetbrains.python.psi.PyFromImportStatement
-import com.jetbrains.python.psi.PyImportElement
+import com.jetbrains.python.psi.PyExpression
 import com.jetbrains.python.psi.impl.PyReferenceExpressionImpl
-import com.jetbrains.python.psi.impl.references.PyImportReference
-import com.jetbrains.python.psi.impl.references.PyQualifiedReference
-import com.jetbrains.python.psi.impl.references.PyReferenceImpl
-import com.jetbrains.python.psi.resolve.PyResolveContext
 import org.pyxy.pyxycharm.highlighter.PyxyAnnotatingVisitor
 
 class PyxyTagName(node: ASTNode) : PyReferenceExpressionImpl(node) {
-    override fun getReference(context: PyResolveContext): PsiPolyVariantReference {
-        val superRef = super.getReference(context)
-        if (superRef.resolve() == null) {
-            // TODO: If there are no . in the expression, resolve as a member of htpy
-        }
-        return superRef
-    }
-
     override fun acceptPyVisitor(pyVisitor: PyElementVisitor) = when (pyVisitor) {
         is PyxyAnnotatingVisitor -> pyVisitor.visitPyxyTagName(this)
         else -> super.acceptPyVisitor(pyVisitor)
